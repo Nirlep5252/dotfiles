e9cfe99ede486e4dc58453f9d30fcb85646933e8
Nirlep5252 <nirlep5252@proton.me>
2023-07-28 21:49:03 +0530

    some neovim config changes

--- lvim/config.lua
+++ lvim/config.lua
@@ -8,4 +8,21 @@ vim.opt.relativenumber = true
 
 -- System clipboard
 -- vim.opt.clipboard = "unnamedplus"
-vim.api.nvim_set_option("clipboard", "unnamedplus")
+-- vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+vim.opt.wrap = true
+lvim.builtin.terminal.open_mapping = "<c-t>"
+
+table.insert(lvim.plugins, {
+  "zbirenbaum/copilot-cmp",
+  event = "InsertEnter",
+  dependencies = { "zbirenbaum/copilot.lua" },
+  config = function()
+    vim.defer_fn(function()
+      require("copilot").setup() -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+      require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+    end, 100)
+  end,
+})
+
+lvim.transparent_window = true
